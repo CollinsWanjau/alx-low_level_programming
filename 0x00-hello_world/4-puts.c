30fce394c7331e580230e0698e1a6414155454ab
colloso999 <tripperskripper@gmail.com>
2022-09-11 19:41:21 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+char str1[50] = "Programming is like building a multilingual puzzle";
+puts(str1);
+return (0);
+}
