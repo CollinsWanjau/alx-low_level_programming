e1fe824d10e6b3c72e55fd5aa5c70aa7cff4101c
colloso999 <tripperskripper@gmail.com>
2022-09-19 12:04:55 -0700

    Added 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+ * reset_to_98: takes a pointer to an int
+ * as parameter
+ * @int *n: variable n a type of pointer
+ * to integer
+ *
+ * Description: Longer description of the function
+ *
+ * Return:nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
