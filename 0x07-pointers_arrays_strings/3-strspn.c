8fb4a767e785d95fefa2105ab870d25dde6a10b3
colloso999 <tripperskripper@gmail.com>
2022-10-04 02:37:23 -0700

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - gets the length of a prefix string
+ * @s: initial segment
+ * @accept: consists only of  bytes
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from
+ * accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	/* return 0 if any one is NULL */
+	if ((s == NULL) || (accept == NULL))
+		return len;
+	/**
+	 * return s position if found in accept
+	 * if not return NULL
+	 */
+	while (*s && strchr(accept, *s++))
+	{
+		len++;
+	}
+	return len;
+}
