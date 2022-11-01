6af886d0dd0f28adcf455aa3bec4853c15bd0090
colloso999 <tripperskripper@gmail.com>
2022-11-01 05:22:55 -0700

    removed whitespace

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,8 +5,7 @@
  * create_array - creates an array of chars,
  * initializes it with a specific char
  * @size: the size of memory to print
- * @c: char to initialize
- * 
+ * @c: char to initialize 
  * Return: Pointer to the array
  */
 char *create_array(unsigned int size, char c)
@@ -15,12 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int position;
 
 	if (size == 0) return (NULL);
-
 	/*initialize buffer with malloc*/
 	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0) return (NULL);
-
 	else
 	{
 		position = 0;
