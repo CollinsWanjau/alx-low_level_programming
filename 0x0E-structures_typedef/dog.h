a8ea68a399db58e5d9489481daab894d1cd38509
colloso999 <tripperskripper@gmail.com>
2022-11-18 16:55:43 +0300

    Modified dog.h by adding preprocessor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-struct dog {
+/**
+ * struct dog - Short description
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Stores the data of a dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-
+#endif
