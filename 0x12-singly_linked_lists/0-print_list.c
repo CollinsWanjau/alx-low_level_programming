73ea1f46001418855ced7c94cf73a057c71b214a
colloso999 <tripperskripper@gmail.com>
2022-12-05 15:13:36 +0300

    added a README file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,11 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 1;
-
 	if (h == NULL)
 		return (0);
 	if (h->str == NULL)
-	{
 		printf("[0] (nil)");
-		return (1);
-	}
 	else
-	{
 		printf("[%d], %s\n", h->len, h->str);
-		count++;
-	}
-
 	return (1 + print_list(h->next));
 }
