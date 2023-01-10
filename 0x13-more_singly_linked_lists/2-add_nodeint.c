9f454fdcc6999b9e2c8ec7647000988a14a83d35
colloso999 <tripperskripper@gmail.com>
2023-01-10 18:43:23 +0300

    modified 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,20 +10,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new;
-/**
- * starts allocating memory for the new node using malloc
- */
 new = malloc(sizeof(listint_t));
 if (new == NULL)
 {
-return NULL;    
+return (NULL);
 }
-/**
- * assigns value of n to the member n of newly allocated node and it sets the
- * next pointer of the new node to point to the current head of the linked list
- */
 new->n = n;
 new->next = *head;
 *head = new;
-return(new);
+return (new);
 }
