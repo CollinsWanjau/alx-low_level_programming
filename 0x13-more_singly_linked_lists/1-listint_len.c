88720ca6e97a3d7190ff3a2279943f435422b91e
colloso999 <tripperskripper@gmail.com>
2023-01-10 16:51:25 +0300

    modified 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,19 @@
 #include "lists.h"
+
+/**
+ * listint_len -  a function that returns number of elements in a linked list
+ * @h: a pointer to head
+ *
+ * Return: count of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
-    const listint_t *temp = h;
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp -> next;
-    }
-    return count;
+size_t count = 0;
+const listint_t *temp = h;
+while (temp != NULL)
+{
+count++;
+temp = temp->next;
+}
+return (count);
 }
