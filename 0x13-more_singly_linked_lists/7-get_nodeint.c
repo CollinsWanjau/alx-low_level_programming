28405ab9baf2410d58d90e60632380bb67e6dcd3
colloso999 <tripperskripper@gmail.com>
2023-01-11 08:28:51 +0300

    a function that retrieves nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,20 +9,19 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i;
-    listint_t *current = head;
-
-    if (head == NULL)
-    {
-        return NULL;
-    }
-    for (i = 0; i < (index); i++)
-    {
-        if (current->next == NULL)
-        {
-            return (NULL);
-        }
-        current = current->next;
-    }
-    return (current);
+unsigned int i;
+listint_t *current = head;
+if (head == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < (index); i++)
+{
+if (current->next == NULL)
+{
+return (NULL);
+}
+current = current->next;
+}
+return (current);
 }
