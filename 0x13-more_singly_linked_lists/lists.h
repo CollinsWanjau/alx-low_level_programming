b7f1851fba970fb76dc89209018add125ee3b1fb
colloso999 <tripperskripper@gmail.com>
2023-01-13 15:40:16 +0300

    modified lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -26,4 +29,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
+int is_palindrome(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
+#endif /* LISTS_H */
