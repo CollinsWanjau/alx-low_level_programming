b728b0191521f1733266fc54ef51ce8f2224611e
colloso999 <tripperskripper@gmail.com>
2023-03-01 06:42:20 +0300

    prints lenght of a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,28 +1,30 @@
 #include "lists.h"
 
 /**
- * dlistint_len - a function that returns length
- * @h: head of the list
- *
- * Return: Length of the list
+ * dlistint_len - returns the number of nodes in a linked
+ *		list
+ * @h: the head of the list
+ * Return: the length of the linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-const dlistint_t *head;
-size_t i = 0;
-head = h;
-if (!head)
-{
-return (0);
-}
-while (head->prev)
-{
-head = head->prev;
-}
-while (head != 0)
-{
-head = head->next;
-i++;
-}
-return (i);
+	const dlistint_t *head;
+	size_t length;
+
+	head = h;
+
+	if (!head)
+		return (0);
+
+	/* get the head of the list */
+	while (head->prev)
+		head = head->prev;
+	/* go through the list now from the head */
+	length = 1;
+	while (head->next)
+	{
+		head = head->next;
+		length++;
+	}
+	return (length);
 }
