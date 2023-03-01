4dd98ea3d2cd0eb75a6ae15c06c8abc9529ab9ac
colloso999 <tripperskripper@gmail.com>
2023-03-01 06:34:40 +0300

    prints lenght of a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,9 +12,13 @@ const dlistint_t *head;
 size_t i = 0;
 head = h;
 if (!head)
-    return (0);
+{
+return (0);
+}
 while (head->prev)
-    head = head->prev;
+{
+head = head->prev;
+}
 while (head != 0)
 {
 head = head->next;
